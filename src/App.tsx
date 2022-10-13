b599b0f131573670ea7bb361ecc0873ae8963dbb
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2022-10-13 12:02:44 +0900

    feat: router 셋업

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
-import './App.css';
+import Header from '@components/Header/Header';
+import Portfolio from '@pages/Portfolio/Portfolio';
+import styled from '@styles/App.module.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <section className={styled.container}>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Portfolio />} />
+        </Routes>
+      </BrowserRouter>
+    </section>
+  );
 }
 
 export default App;
