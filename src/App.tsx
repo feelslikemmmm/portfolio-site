44548b460bfd7affddcbcab9d7702368c5d9ec24
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2022-10-14 09:32:15 +0900

    feat: github pages 배포

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import Header from '@components/header/Header';
 import Portfolio from '@pages/portfolio/Portpolio';
 import styled from '@styles/App.module.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <section className={styled.container}>
       <Header />
-      <BrowserRouter>
+      <Router>
         <Routes>
           <Route path="/" element={<Portfolio />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </section>
   );
 }
