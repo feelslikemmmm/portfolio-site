fec8c18eaf9703658a12eb34b701750c363dd6d5
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2022-10-17 14:05:18 +0900

    feat: projects description update

--- src/util/projects.ts
+++ src/util/projects.ts
@@ -7,11 +7,10 @@ export const projects = [
     id: 1,
     main: 'card makers',
     title: '명함 제작 웹 어플리케이션',
-    description: `Front-end : React-Hook, Firebase, Module-CSS Back-end : Firebase
-    Realtime Database, Cloudinary Deploy : Netlify Role: Full stack`,
     front: 'Front-end : React-Hook, Firebase, Module-CSS',
     back: 'Back-end : Firebase Realtime Database, Cloudinary',
     deploy: 'Deploy : Netlify',
+    role: `Role: Full stack`,
     src: 'https://cardmakers.netlify.app/',
     git: 'https://github.com/feelslikemmmm/card_maker',
     images: CardMakers,
@@ -21,13 +20,10 @@ export const projects = [
     id: 2,
     main: '모두의 이력서',
     title: '취준생을 위한 이력서 작성 플랫폼',
-    description: `Front-end : React, Redux, Redux-saga, Typescript, Module-CSS
-    Back-end : Node.js, Express , MongoDB, JWT, Multer
-    Deploy : AWS(S3, CloudFront, Route53, EC2, Load Balancer)
-    Role: Front-end`,
     front: 'Front-end : React, Redux, Redux-saga, Typescript, Module-CSS',
     back: 'Back-end : Node.js, Express , MongoDB, JWT, Multer',
     deploy: 'Deploy : AWS(S3, CloudFront, Route53, EC2, Load Balancer)',
+    role: 'Role: Front-end',
     src: 'https://everymoi.com/',
     git: 'https://github.com/codestates/Moi_Client',
     images: Moi,
@@ -37,12 +33,9 @@ export const projects = [
     id: 3,
     main: 'airmug pro',
     title: '인터랙션을 활용한 web site',
-    description: `Front-end : Javascript
-    Deploy : Netlify
-    Role: Front-end
-    `,
     front: 'Front-end : Javascript',
     deploy: 'Deploy : Netlify',
+    role: `Role: Front-end`,
     src: 'https://airmug-pro.netlify.app/',
     git: 'https://github.com/feelslikemmmm/Apple-InteractiveWebProject',
     images: AirMugPro,
@@ -52,11 +45,9 @@ export const projects = [
     id: 4,
     main: 'find carrot',
     title: '벌레를 피해 당근 수확하기',
-    description: `Front-end : Javascript
-    Deploy : Netlify
-    Role: Front-end`,
     front: 'Front-end : React',
     deploy: 'Deploy : Netlify',
+    role: `Role: Front-end`,
     src: 'https://find-carrot.netlify.app',
     git: 'https://github.com/feelslikemmmm/fun_game',
     images: FindCarrot,
